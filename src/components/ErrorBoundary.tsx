ca31073a9a806e0c61832b086761648d63d46230
Asrul Kadir <asrulkadir.asus4@gmail.com>
2024-12-09 01:31:50 +0800

    fix error boundary

--- src/components/ErrorBoundary.tsx
+++ src/components/ErrorBoundary.tsx
@@ -1,36 +1,23 @@
-import { ReactNode, useState, useEffect } from 'react';
+import { ReactNode } from 'react';
+import { ErrorBoundary as ReactErrorBoundary } from 'react-error-boundary';
 
 interface Props {
   children: ReactNode;
 }
 
+const ErrorFallback = () => (
+  <div className='flex flex-col justify-center items-center h-screen'>
+    <h1 className='text-2xl text-center mt-4'>Something went wrong.</h1>
+    <p className='text-center'>Please try again later.</p>
+  </div>
+);
+
 const ErrorBoundary: React.FC<Props> = ({ children }) => {
-  const [hasError, setHasError] = useState(false);
-
-  useEffect(() => {
-    const handleError = () => {
-      setHasError(true);
-    };
-
-    window.addEventListener('error', handleError);
-
-    return () => {
-      window.removeEventListener('error', handleError);
-    };
-  }, []);
-
-  if (hasError) {
-    return (
-      <div
-        className='flex flex-col justify-center items-center h-screen'
-      >
-        <h1 className='text-2xl text-center mt-4'>Something went wrong.</h1>
-        <p className='text-center'>Please try again later.</p>
-      </div>
-    )
-  }
-
-  return <>{children}</>;
+  return (
+    <ReactErrorBoundary FallbackComponent={ErrorFallback}>
+      {children}
+    </ReactErrorBoundary>
+  );
 };
 
 export default ErrorBoundary;
